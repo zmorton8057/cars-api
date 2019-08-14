6e5aa58e1bf8a64b65c7e39298d83d249ea2ab6e
Zac Morton <zmorton8057@gmail.com>
2019-08-14 15:11:39 -0700

    cleaned up route handlers for simplicity and modularization

--- routes/cars/single.js
+++ routes/cars/single.js
@@ -1,8 +1,7 @@
 const data = require('../../data.json');
 
 module.exports = (req, res) => {
-    const cardId = req.params.carId * 1;
-    const car = data.cars.find(c => c.id === cardId)
+    const car = req.car;
 
     res.status(200).json({car})
 }
